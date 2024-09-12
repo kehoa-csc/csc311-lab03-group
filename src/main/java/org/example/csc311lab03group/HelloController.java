46bedc480a6a1487c936db333c7b95b2c6b300b3
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2024-09-12 19:21:01 -0400

    add button to move robot

--- src/main/java/org/example/csc311lab03group/HelloController.java
+++ src/main/java/org/example/csc311lab03group/HelloController.java
@@ -1,15 +1,24 @@
 package org.example.csc311lab03group;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 
 public class HelloController {
     @FXML
     private ImageView robot;
-
     @FXML
     private ImageView robetMaze;
+
+    @FXML
+    void MoveRobot(ActionEvent event) {
+        //double x = robot.getX();
+        //robot.setX(x+1);
+
+
+    }
 
 
 }
