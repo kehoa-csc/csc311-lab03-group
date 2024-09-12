a85ecc39556a933f4fa5ab07d1960cc93b259635
neomodulo <157737128+andrewkehoe@users.noreply.github.com>
2024-09-12 18:11:00 -0400

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.csc311lab03group {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.csc311lab03group to javafx.fxml;
+    exports org.example.csc311lab03group;
+}
