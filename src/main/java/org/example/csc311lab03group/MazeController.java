7d438f41d069f543f6e7ef9988344133afa32038
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2024-09-16 16:51:04 -0400

    rename the file as maze add findPath to find path

--- src/main/java/org/example/csc311lab03group/MazeController.java
+++ src/main/java/org/example/csc311lab03group/MazeController.java
@@ -2,17 +2,15 @@ package org.example.csc311lab03group;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.util.Arrays;
 
-public class HelloController {
+public class MazeController {
     @FXML
     private ImageView robot;
     @FXML
@@ -43,7 +41,7 @@ public class HelloController {
         colorText.setText(x+","+y+":" +color.toString());
 
          */
-        test1();
+        //test1();
     }
 
     // test class, no need to care
@@ -52,6 +50,16 @@ public class HelloController {
         int[][] mazeMap = imageTo2DArray(maze);
 
        System.out.println(Arrays.deepToString(mazeMap));
+    }
+
+    void test2(){
+        Image maze = robetMaze.getImage();
+        int[][] mazeMap = imageTo2DArray(maze);
+
+        int x = (int)robot.getX();
+        int y = (int)robot.getY();
+
+
     }
 
     // translate the maze image to be a 2D Array, 0 is the path, 1 is the wall
@@ -79,6 +87,55 @@ public class HelloController {
         }
         return maze;
     }
+
+    // find the path in maze
+    boolean findPath(int[][] maze, int x,int y,int targetX, int targetY, boolean[][] visited) {
+        // define as up , down, right,left
+        final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        if(x == targetX && x == targetY){
+            setFinder(x, y);
+            return  true;
+        }
+
+        visited[x][y] = true; // mark as passing
+
+        for(int[] direction:DIRECTIONS){
+            int newX = x+direction[0];
+            int newY = y+direction[1];
+
+            // check new coordinate is valid
+            if(isVaild(maze,newX,newY,visited)){
+                if(findPath(maze,newX,newY,targetX,targetY,visited)){
+                    return  true;
+                }
+            }
+        }
+
+
+        return false;
+
+    }
+
+    // set the robot going
+    void setFinder(int x, int y) {
+        try {
+            robot.setX(x);
+            robot.setY(y);
+            Thread.sleep(100);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    boolean isVaild(int[][]maze, int x, int y, boolean[][]visited){
+        int n = maze.length;
+        // check it is in range , it is in the path(0 is path in maze), and it is visited
+        boolean result = (x >= 0 && y >= 0 &&x < n && y < n)&& (maze[x][y] == 0) && (visited[x][y]);
+        return  result;
+    }
+
 
 
 }
