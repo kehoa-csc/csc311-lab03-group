ebc3bd5b7d706660ca8d92c7ba5fc1e64ac45d73
neomodulo <157737128+andrewkehoe@users.noreply.github.com>
2024-09-16 13:58:47 -0400

    animation works :)

--- src/main/java/org/example/csc311lab03group/HelloController.java
+++ src/main/java/org/example/csc311lab03group/HelloController.java
@@ -2,6 +2,7 @@ package org.example.csc311lab03group;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -24,28 +25,29 @@ public class HelloController {
 
     public int curr = 0;
 
+
     private double[][] robotPath = {
         {35,215},{35,120},{220,120},{220,75},{267,75},{267,265},{315,265},{315,170},{410,170},{410,80},{460,80}
             ,{460,200},{550,200}
     };
 
+    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+                    double nextX = robotPath[curr][0];
+                    double nextY = robotPath[curr][1];
 
-
+                    robot.setLayoutX(nextX);
+                    robot.setLayoutY(nextY);
+                    curr++;
+        }
+    }));
 
     @FXML
-    public void robotMov() throws InterruptedException {
+    public void robotMov() {
+        timeline.setCycleCount(robotPath.length);
+        timeline.play();
 
-        for(int i=0; i< 13;i++) {
-
-            if (curr < robotPath.length) {
-                double nextX = robotPath[curr][0];
-                double nextY = robotPath[curr][1];
-
-                robot.setLayoutX(nextX);
-                robot.setLayoutY(nextY);
-                curr++;
-            }
-        }
     }
 
 
