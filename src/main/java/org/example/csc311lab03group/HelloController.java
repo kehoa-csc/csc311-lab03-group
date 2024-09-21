fbbf9d80774f8fc5f65f51fffed060ea227d36d9
Ben2474 <bennettthomas24@gmail.com>
2024-09-20 20:06:40 -0400

    Used PixelReader to move and initialize car.

--- src/main/java/org/example/csc311lab03group/HelloController.java
+++ src/main/java/org/example/csc311lab03group/HelloController.java
@@ -6,40 +6,55 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.event.ActionEvent;
+import javafx.scene.image.PixelReader;
 import javafx.util.Duration;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 
 public class HelloController {
     @FXML
     private ImageView robot;
 
-//    @FXML
-//    private Button robotStart;
+    @FXML
+    private Button carStart;
+
+    @FXML
+    private Button robotStart;
 
     @FXML
     private ImageView robetMaze;
 
-   // private Droid droid;
+    @FXML
+    private ImageView carImg;
+
+    @FXML
+    private TabPane tabPn;
 
     public int curr = 0;
-
+    private Car car;
 
     private double[][] robotPath = {
-        {35,215},{35,120},{220,120},{220,75},{267,75},{267,265},{315,265},{315,170},{410,170},{410,80},{460,80}
+            {35,215},{35,120},{220,120},{220,75},{267,75},{267,265},{315,265},{315,170},{410,170},{410,80},{460,80}
             ,{460,200},{550,200}
     };
 
-    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+
+    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
-                    double nextX = robotPath[curr][0];
-                    double nextY = robotPath[curr][1];
+            double nextX = robotPath[curr][0];
+            double nextY = robotPath[curr][1];
 
-                    robot.setLayoutX(nextX);
-                    robot.setLayoutY(nextY);
-                    curr++;
+            robot.setLayoutX(nextX);
+            robot.setLayoutY(nextY);
+            curr++;
         }
     }));
 
@@ -47,9 +62,19 @@ public class HelloController {
     public void robotMov() {
         timeline.setCycleCount(robotPath.length);
         timeline.play();
-
+        robotStart.setDisable(true);
     }
 
-
+    @FXML
+    public void makeCar() {
+        //car = new Car(carImg,timeline);
+        carStart.setFocusTraversable(false);
+        tabPn.setFocusTraversable(false);
+        tabPn.setDisable(true);
+    }
+    // initializes car with PixelReader
+    public void carInitialized(PixelReader pr) {
+        this.car = new Car(carImg, timeline, pr);
+    }
 
 }
